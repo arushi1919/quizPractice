f0134f07fe4626c17b7711a5d42847e7e356118e
Arushi Ravula <arushi.ravula@gmail.com>
2023-12-04 23:06:53 -0500

    method written

--- Main.java
+++ Main.java
@@ -1,9 +1,19 @@
 public class Main{    
     public static void main(String[]Args){
-
-        for(int i=1;i<11;i++){
+        int i=1;
+        while(i<11){
             System.out.println(i);
         }
+
     }
+
+     public String characterTask(int num){
+            String s="";
+            while(s.length()<10){
+                s=" "+s;
+            }
+        
+            return s;
+        }
 }
 
